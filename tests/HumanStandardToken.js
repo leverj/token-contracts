78c8ca83ec2b7e5127b1b11367ed9a4a05627c94
stuart <stuarth323@gmail.com>
2017-10-17 18:33:28 -0400

    added more tests

--- tests/HumanStandardToken.js
+++ tests/HumanStandardToken.js
@@ -7,7 +7,7 @@ var web3            = new Web3(new Web3.providers.HttpProvider('http://localhost
 describe('HumanStandardToken', function () {
 	describe('HumanStandardToken deployment', function () {
 		it('Verifying that the HumanStandardToken can be initialized, and all initial variables are set', async () => {
-            let contract = await token.deployHumanStandardToken();
+            let contract = await token.deployHumanStandardToken(100,'ConsenSys',10,'CS',"0x5b1869d9a4c187f2eaa108f3062412ecf0526b24",20);
             let name = await contract.name();
             let symbol = await contract.symbol();
             let decimals = await contract.decimals();
@@ -23,50 +23,35 @@ describe('HumanStandardToken', function () {
 
     describe("HumanStandardToken transfer function check", function (){
         it('Verifying that the HumanStandardToken transfer function is working as intended', async () => {
-            let deploy = await token.deployHumanStandardToken();
-            let transfer = await token.transferHumanStandardToken(deploy);
+            let deploy = await token.deployHumanStandardToken(100,'ConsenSys',10,'CS',"0x5b1869d9a4c187f2eaa108f3062412ecf0526b24",20);
+            let transfer = await token.transferHumanStandardToken(deploy, web3.eth.accounts[1], 40);
             let balance0  = await deploy.balanceOf(web3.eth.accounts[0]);
             let balance1  = await deploy.balanceOf(web3.eth.accounts[1]);
             assert(JSON.parse(balance0) === 60, "Balances not being transfered properly from account[0]");
             assert(JSON.parse(balance1) === 40, "Balances not being transfered properly from account[1]");
-        }).timeout(300000)
+        }).timeout(300000);
     })
 
-    // describe("HumanStandardToken approve/allowance function check", function(){
-    //     it("Verifying that the HumanStandardToken approve/allowance is working as intended", async () => {
-    //         let contract = await token.deployHumanStandardToken();
+    describe("HumanStandardToken approve/allowance function checks", function(){
+        it("Verifying that the HumanStandardToken approve/allowance is working as intended", async () => {
+            let contract = await token.deployHumanStandardToken(100,'ConsenSys',10,'CS',"0x5b1869d9a4c187f2eaa108f3062412ecf0526b24",20);
+            let approve = await token.approveHumanStandardToken(contract, web3.eth.accounts[2], 60);
+            let amount = await contract.allowance(web3.eth.accounts[0], web3.eth.accounts[2]);
+            assert(JSON.parse(amount) === 60, "Correct amount not being approved");
+        }).timeout(300000);
+    })
 
-    //     })
-    // })
+    describe("HumanStandardToken approve/transferFrom function checks", function(){
+        it("Verifying that the HumanStandardToken approve/transferFrom functions are working properly", async () => {
+            let contract = await token.deployHumanStandardToken(100,'ConsenSys',10,'CS',"0x5b1869d9a4c187f2eaa108f3062412ecf0526b24",20);
+            let approve = await token.approveHumanStandardToken(contract, web3.eth.accounts[2], 60);
+            let transferFrom = await token.transferFromHumanStandardToken(contract, web3.eth.accounts[0], web3.eth.accounts[1], 60, web3.eth.accounts[2]);
+            let postAmount = await contract.allowance(web3.eth.accounts[0], web3.eth.accounts[2]);
+            let postBalance1 = await contract.balanceOf(web3.eth.accounts[1]);
+            let postBalance0 = await contract.balanceOf(web3.eth.accounts[0]);
+            assert(JSON.parse(postAmount) === 0);
+            assert(JSON.parse(postBalance1) === 60);
+            assert(JSON.parse(postBalance0) === 40)
+        }).timeout(300000);
+    })
 })
-
-
-//contract('HumanStandardToken', function(accounts) {
-
-    // it("Verifying the approve/allowance from function works correct", async () => {
-    //     let contract = await HumanStandardToken.new(100,'ConsenSys',10,'CS','0x5b1869d9a4c187f2eaa108f3062412ecf0526b24',20);
-    //     let approve = await contract.approve(accounts[2], 60);
-    //     let amount = await contract.allowance(accounts[0], accounts[2]);
-    //     assert(JSON.parse(amount) === 60);
-    // })
-
-    // it("Verifying the approve from/ transfer from function works correctly", async () => {
-    //     let contract = await HumanStandardToken.new(100,'ConsenSys',10,'CS','0x5b1869d9a4c187f2eaa108f3062412ecf0526b24',20);
-    //     let approve = await contract.approve(accounts[2], 60);
-    //     let transferFrom = await contract.transferFrom(accounts[0], accounts[1], 60,{ from: accounts[2] });
-    //     let postAmount = await contract.allowance(accounts[0], accounts[2]);
-    //     let postBalance1 = await contract.balanceOf(accounts[1]);
-    //     let postBalance0 = await contract.balanceOf(accounts[0]);
-    //     assert(JSON.parse(postAmount) === 0);
-    //     assert(JSON.parse(postBalance1) === 60);
-    //     assert(JSON.parse(postBalance0) === 40)
-    // })
-
-
-    // it("Checking initial amount uint256 against any size number", async () => {
-    //     let contract = await HumanStandardToken.new(-10,'ConsenSys',10,'CS','0x5b1869d9a4c187f2eaa108f3062412ecf0526b24',20);
-    //     let balance  = await contract.balanceOf(accounts[0]);
-    //     console.log(balance)
-    //     //assert(JSON.parse(balance) === 100000000000000000000000000000000000000000000000000000000000000000000000000000, "Balances not being set properly");        
-    // })
-//})

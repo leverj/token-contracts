5e841e2e91d2277048ac1d360d2b62a61ee2945b
jsonsivar <jsonsivar@gmail.com>
2017-10-17 14:15:36 -0400

    some more additions

--- tests/Sale.js
+++ tests/Sale.js
@@ -1,27 +1,5 @@
-var build = require('../dist/Sale');
+ar assert = require('assert');
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var MultiSigWallet = require('../lib/Sale');
 
-
-module.exports = function (owners, required, opts) {
-	var opts = opts || {};
-	opts.from = opts.from || web3.eth.accounts[0];
-	opts.value = opts.value || 0;
-	var factory = web3.eth.contract(build.abi);
-	return factory.new({
-		from: opts.from,
-		value: opts.value,
-		data: build.bin,
-		gas: web3.eth.estimateGas({data: build.bin})
-	}, function (err, contract) {
-		if (!err) {
-			if (!contract.address)
-				console.log(`Transaction sent. Hash: ${contract.transactionHash}`);
-			else
-				console.log(`Transaction mined. Address: ${contract.address}`);
-		}
-		else {
-			console.log(`err: ${err}`);
-		}
-	});
-}

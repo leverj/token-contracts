ce755177366716cc9d0ce608cedc8ff983e283ef
jsonsivar <jsonsivar@gmail.com>
2017-10-20 08:27:18 -0400

    adding testing related stuff

---
+++ test-documentation.md
@@ -0,0 +1,155 @@
+# Unit Tests
+
+## Sale
+
+### Creation
+`function Sale(address _owner, uint _freezeBlock, uint _startBlock, uint _endBlock)`
+- create with "real" inputs
+- create with owner=0
+- create with owner invalid
+- create with freezeBlock=0
+- create with freezeblock < block.number
+- create with freezeblock > startblock
+- create with freezeblock > endblock
+- create with freezeblock = startblock
+- create with freezeblock = endblock
+- create with underflow freezeblock
+- create with overflow freezeblock
+- create with low freezeblock
+- create with high freezeblock
+- create with startBlock=0
+- create with startblock < block.number
+- create with startblock > endblock
+- create with startblock = endblock
+- create with negative startblock
+- create with underflow startblock
+- create with overflow startblock
+- create with low startblock
+- create with high startblock
+- create with endBlock=0
+- create with endblock < block.number
+- create with underflow startblock
+- create with overflow startblock
+- create with low endblock
+- create with high endblock
+- create with negative endblock
+
+
+### Distribute Time Locks
+`function distributeTimeLockedTokens(address[] _beneficiaries, uint[] _beneficiaryTokens, uint[] _timelocks, uint[] _breakdown)`
+- call with "real" founder inputs
+- call with "real" partner inputs
+- call as non-owner
+- call with "real" liquidity inputs
+- call with "real" presale inputs
+- call with no beneficiaries
+- call with no beneficiaryTokes
+- call with no timelocks
+- call with no breakdown
+- call with beneficiaries.length > 50
+- call with beneficiaryTokess.length > 50
+- call with timelocks.length > 50
+- call with breakdown.length > 50
+- call with beneficiary.length != tokens.length
+- call with timelock.length != breakdown.length
+- call until max is hit
+- call in parallel
+- call after setup complete
+- call with single tranch
+- call with multiple tranch
+- call with even breakdowns
+- call with lobsided breakdowns
+- call with overflow beneficiarytokens
+- call with underflow beneficiarytokens
+- call with overflow timelocks
+- call with underflow timelocks
+- call with overflow breakdowns
+- call with underflow breakdowns
+
+### Configure Wallet
+`function configureWallet(address _wallet)`
+- call as owner
+- call as non-owner
+- call with address 0
+- call with valid address
+
+
+### Add Whitelist    
+`function addWhitelist(address[] _purchaser, uint[] _amount)`
+- call as owner
+- call as non-owner
+- call with address 0
+- call with array lengths not equal
+- call with amount 0
+- call with amount under 1eth worth
+- call with amount over 500eth worh
+- call with underflow amount
+- call with overflow amount
+- call with purchaser.length > 50
+- call with amount.length > 50
+
+### Setup Complete
+`function setSetupComplete()`
+
+### Change Owner
+ `function changeOwner(address _newOwner)`
+- call with same owner
+- call with owner 0
+- call with new owner
+
+### Change Price
+`function changePrice(uint _newPrice)`
+- call with price 0
+- call with low price
+- call with high price
+
+### Change Start Block
+`function changeStartBlock(uint _newBlock)`
+- create with newBlock=0
+- create with newBlock < block.number
+- create with newBlock > endblock
+- create with newBlock = endblock
+- create with negative newBlock
+
+### Toggle Emergency
+`function emergencyToggle()`
+- call before frozen
+- call on frozen block
+- call after frozen before start
+- call after start before end
+- call after end
+
+### Purchase
+`purchase()`
+- call before setup complete
+- call without emergency
+- call with emergency
+- call before frozen
+- call on frozen block
+- call after frozen before start
+- call after start before end
+- call after end
+
+- call without ether
+- call with even amount
+- call with uneven amount
+
+### Lock Unsold
+`lockUnsold()`
+- call before setup complete
+- call after setup complete
+- call before sale ends
+- call before sale ends
+
+## Token
+STU to add based on his tests
+
+## Multisig
+STU to add based on his tests
+
+## Filter
+TBC
+
+## Disbursement
+TBC
+
--- tests/Sale.js
+++ tests/Sale.js
@@ -4,62 +4,263 @@ var assert          			= require('assert');
 var Web3            			= require('web3');
 var web3            			= new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-describe('Sale tests', () => {
-	describe('Sale very simple happy path', ()  =>{
-		it('Verifying that the sale can be initialized, and all initial variables are set', async () => {
+describe('sale unit tests', () => {
 
-			var owner = web3.eth.accounts[0];
-			var supply = 1000000000;
-			var name = "leverj";
-			var decimals = 9;
-			var symbol = "LEV";
-			var freezeBlock = web3.eth.blockNumber + 10;
-			var startBlock = freezeBlock + 1;
-			var endblock = startBlock + 1000000000000000;
-			var price = 333333333333333; //price in wei
+	describe('constructor', () => {
 
-            let testSale = await sale.createSale(owner, supply, name, decimals, symbol, freezeBlock, startBlock, endblock, price);
- 
-            const actualStartBlock = await testSale.startBlock();
-            assert(startBlock === JSON.parse(actualStartBlock), "start block not saved properly");
-            // TODO: check all remaining parameters
+		var testSale;
+		var owner = web3.eth.accounts[0];
+		var freezeBlock = 4500082;
+		var startBlock = freezeBlock + 10000;
+		var endblock = startBlock + 170000;
 
-            let tokenAddress = await testSale.token();
+		describe('creation of sale contract with valid parameters', () => {
+
+            const expectedTOTAL_SUPPLY = 1000000000;
+    		const expectedMAX_PRIVATE = 750000000;
+		    const expectedMULTIPLIER = 9;
+		    const expectedNAME = "Leverj";
+		    const expectedSYMBOL = "LEV";
+		    const expectedprice_inwei = 333333333333333;
+		    const expectedprivateAllocated = 0;
+		    const expectedSetupCompleteFlag = false;
+		    const expectedEmergencyFlag = false;
+
+			it('sale should have the correct parameters saved', async () => {
+				testSale = await sale.createSale(owner, freezeBlock, startBlock, endblock);
+				let actualOwner =  testSale.owner();
+				let actualFreezeBlock =  testSale.freezeBlock();
+				let actualStartBlock =  testSale.startBlock();
+				let actualEndBlock =  testSale.endBlock();
+				let actualTOTAL_SUPPLY =  testSale.TOTAL_SUPPLY();
+				let actualMAX_PRIVATE =  testSale.MAX_PRIVATE();
+				let actualMULTIPLIER =  testSale.MULTIPLIER();
+				let actualNAME =  testSale.NAME();
+				let actualSYMBOL =  testSale.SYMBOL();
+				let actualprice_inwei =  testSale.price_in_wei();
+				let actualprivateAllocated =  testSale.privateAllocated();
+				let actualSetupCompleteFlag =  testSale.setupCompleteFlag();
+				let actualEmergencyFlag =  testSale.emergencyFlag();
+
+				assert(owner === actualOwner, "owner not saved properly");
+				assert(freezeBlock === JSON.parse(actualFreezeBlock), "freezeBlock not saved properly");
+				assert(startBlock === JSON.parse(actualStartBlock), "starBlock not saved properly");
+				assert(endblock === JSON.parse(actualEndBlock), "endBlock not saved properly");
+				assert(expectedTOTAL_SUPPLY === JSON.parse(actualTOTAL_SUPPLY), "total supply not saved properly");
+				assert(expectedMAX_PRIVATE === JSON.parse(actualMAX_PRIVATE), "max private not saved properly");
+				assert(expectedMULTIPLIER === JSON.parse(actualMULTIPLIER), "multiplier not saved properly");
+				assert(expectedNAME === actualNAME, "name not saved properly");
+				assert(expectedSYMBOL === actualSYMBOL, "symbol not saved properly");
+				assert(expectedprice_inwei === JSON.parse(actualprice_inwei), "price in wei not saved properly");
+				assert(expectedprivateAllocated === JSON.parse(actualprivateAllocated), "private allocated not set properly");
+				assert(expectedSetupCompleteFlag === JSON.parse(actualSetupCompleteFlag), "setupcompleteflag block not saved properly");
+				assert(expectedEmergencyFlag === JSON.parse(actualEmergencyFlag), "emergency flag not saved properly");
+			}).timeout(300000);
+
+			it('underlying token should have the correct parameters saved', async () => {
+				let tokenAddress = await testSale.token();
+	            let token = await HumanStandardToken.at(tokenAddress);
+				const expectedVersion = 'H0.1';
+				const expectedTotalSupply = expectedTOTAL_SUPPLY*(Math.pow(10, expectedMULTIPLIER));
+				const expectedBalanceOfSale = 1000000000000000000;
+				const expectedSaleAddress = testSale.address;
+				const expectedEndBlock = endblock;
+
+				let actualName = token.name();
+	            let actualDecimal = token.decimals();
+	            let actualSymbol = token.symbol();
+	            let actualVersion = token.version();
+	            let actualTotalSupply = token.totalSupply();
+	            let actualBalanceOfSale = token.balanceOf(testSale.address);
+	            let actualSaleAddress = token.sale();
+	            let actualEndBlock = token.endBlock();
+				
+				assert(expectedNAME === actualName, "name not saved properly");
+				assert(expectedMULTIPLIER === JSON.parse(actualDecimal), "decimal not saved properly");
+				assert(expectedSYMBOL === actualSymbol, "symbol not saved properly");
+				assert(expectedVersion === actualVersion, "version not saved properly");
+				assert(expectedTotalSupply === JSON.parse(actualTotalSupply), "total supply not saved properly");
+				assert(expectedBalanceOfSale === JSON.parse(actualBalanceOfSale), "balance of sale not set properly");
+				assert(expectedSaleAddress === actualSaleAddress, "sale address not saved properly");
+				assert(expectedEndBlock === JSON.parse(actualEndBlock), "end block not saved properly");
+
+			}).timeout(300000);
+
+		});
+
+		/* remaining describes:
+		- create with owner=0
+		- create with owner invalid
+		- create with freezeBlock=0
+		- create with freezeblock < block.number
+		- create with freezeblock > startblock
+		- create with freezeblock > endblock
+		- create with freezeblock = startblock
+		- create with freezeblock = endblock
+		- create with underflow freezeblock
+		- create with overflow freezeblock
+		- create with low freezeblock
+		- create with high freezeblock
+		- create with startBlock=0
+		- create with startblock < block.number
+		- create with startblock > endblock
+		- create with startblock = endblock
+		- create with negative startblock
+		- create with underflow startblock
+		- create with overflow startblock
+		- create with low startblock
+		- create with high startblock
+		- create with endBlock=0
+		- create with endblock < block.number
+		- create with underflow startblock
+		- create with overflow startblock
+		- create with low endblock
+		- create with high endblock
+		- create with negative endblock
+		*/
+
+	});
+
+});
+
+describe('sale end-to-end test', () => {
+
+	var testSale;
+	var owner = web3.eth.accounts[0];
+	var freezeBlock = 4500082;
+	var startBlock = freezeBlock + 10000;
+	var endblock = startBlock + 170000;
+
+	//create
+	describe('creation of sale contract with real LEV parameters', () => {
+
+        const expectedTOTAL_SUPPLY = 1000000000;
+		const expectedMAX_PRIVATE = 750000000;
+	    const expectedMULTIPLIER = 9;
+	    const expectedNAME = "Leverj";
+	    const expectedSYMBOL = "LEV";
+	    const expectedprice_inwei = 333333333333333;
+	    const expectedprivateAllocated = 0;
+	    const expectedSetupCompleteFlag = false;
+	    const expectedEmergencyFlag = false;
+
+		it('sale should have the correct parameters saved', async () => {
+			testSale = await sale.createSale(owner, freezeBlock, startBlock, endblock);
+			let actualOwner =  testSale.owner();
+			let actualFreezeBlock =  testSale.freezeBlock();
+			let actualStartBlock =  testSale.startBlock();
+			let actualEndBlock =  testSale.endBlock();
+			let actualTOTAL_SUPPLY =  testSale.TOTAL_SUPPLY();
+			let actualMAX_PRIVATE =  testSale.MAX_PRIVATE();
+			let actualMULTIPLIER =  testSale.MULTIPLIER();
+			let actualNAME =  testSale.NAME();
+			let actualSYMBOL =  testSale.SYMBOL();
+			let actualprice_inwei =  testSale.price_in_wei();
+			let actualprivateAllocated =  testSale.privateAllocated();
+			let actualSetupCompleteFlag =  testSale.setupCompleteFlag();
+			let actualEmergencyFlag =  testSale.emergencyFlag();
+
+			assert(owner === actualOwner, "owner not saved properly");
+			assert(freezeBlock === JSON.parse(actualFreezeBlock), "freezeBlock not saved properly");
+			assert(startBlock === JSON.parse(actualStartBlock), "starBlock not saved properly");
+			assert(endblock === JSON.parse(actualEndBlock), "endBlock not saved properly");
+			assert(expectedTOTAL_SUPPLY === JSON.parse(actualTOTAL_SUPPLY), "total supply not saved properly");
+			assert(expectedMAX_PRIVATE === JSON.parse(actualMAX_PRIVATE), "max private not saved properly");
+			assert(expectedMULTIPLIER === JSON.parse(actualMULTIPLIER), "multiplier not saved properly");
+			assert(expectedNAME === actualNAME, "name not saved properly");
+			assert(expectedSYMBOL === actualSYMBOL, "symbol not saved properly");
+			assert(expectedprice_inwei === JSON.parse(actualprice_inwei), "price in wei not saved properly");
+			assert(expectedprivateAllocated === JSON.parse(actualprivateAllocated), "private allocated not set properly");
+			assert(expectedSetupCompleteFlag === JSON.parse(actualSetupCompleteFlag), "setupcompleteflag block not saved properly");
+			assert(expectedEmergencyFlag === JSON.parse(actualEmergencyFlag), "emergency flag not saved properly");
+		}).timeout(300000);
+
+		it('underlying token should have the correct parameters saved', async () => {
+			let tokenAddress = await testSale.token();
             let token = await HumanStandardToken.at(tokenAddress);
-            let saleBalance = await token.balanceOf(testSale.address);
-            assert(supply === JSON.parse(saleBalance), "sale contracts initial balance is NOT  token supply");
+			const expectedVersion = 'H0.1';
+			const expectedTotalSupply = expectedTOTAL_SUPPLY*(Math.pow(10, expectedMULTIPLIER));
+			const expectedBalanceOfSale = 1000000000000000000;
+			const expectedSaleAddress = testSale.address;
+			const expectedEndBlock = endblock;
 
-            
-            let arr1 = [];
-            let t1 = sale.distributeFoundersTokens(testSale.address, arr1, arr1, arr1, arr1);
-            let t2 = sale.distributeLiquidityTokens(testSale.address, arr1, arr1);
-            let t3 = sale.distributePartnersTokens(testSale.address, arr1, arr1, arr1, arr1);
-            let t4 = sale.distributePresaleTokens(testSale.address, arr1, arr1, arr1, arr1);
-            let t5 = sale.configureWallet(testSale.address, web3.eth.accounts[2]); 
-            let results = [await t1, await t2, await t3, await t4, await t5];
-
-            let actualWallet = testSale.wallet();
-            assert(actualWallet === web3.eth.accounts[2], "wallet configuration is not correct");
-
-            // TODO: replace this with an event watcher :)
-            while(web3.eth.blockNumber < startBlock){}
-
-            let t6 = await sale.purchase(testSale.address, web3.eth.accounts[3], 333333333333333);
-            let newtokenBalance = await token.balanceOf(web3.eth.accounts[3]);
-            assert(JSON.parse(newtokenBalance) === 1, "incorrect amount of token balance for purchaser");
-            
-            /* not yet ...
-            const amountToTransfer = 50000;
-            await HumanStandardToken.transferHumanStandardToken(token, web3.eth.accounts[1], amountToTransfer);
-            let anotherBalance = await token.balanceOf(web3.eth.accounts[1]);
-            console.log("token address: " + token.address);
-            saleBalance = await token.balanceOf(testSale.address);
-            console.log("new balance: " + JSON.parse(saleBalance));
-            assert(JSON.parse(anotherBalance) === amountToTransfer, "transferred amount didn't show up on recipients balance");
-            assert(JSON.parse(saleBalance) === (supply - amountToTransfe), "transferred amount didn't deduct from sale contracts balance");
-            */
+			let actualName = token.name();
+            let actualDecimal = token.decimals();
+            let actualSymbol = token.symbol();
+            let actualVersion = token.version();
+            let actualTotalSupply = token.totalSupply();
+            let actualBalanceOfSale = token.balanceOf(testSale.address);
+            let actualSaleAddress = token.sale();
+            let actualEndBlock = token.endBlock();
+			
+			assert(expectedNAME === actualName, "name not saved properly");
+			assert(expectedMULTIPLIER === JSON.parse(actualDecimal), "decimal not saved properly");
+			assert(expectedSYMBOL === actualSymbol, "symbol not saved properly");
+			assert(expectedVersion === actualVersion, "version not saved properly");
+			assert(expectedTotalSupply === JSON.parse(actualTotalSupply), "total supply not saved properly");
+			assert(expectedBalanceOfSale === JSON.parse(actualBalanceOfSale), "balance of sale not set properly");
+			assert(expectedSaleAddress === actualSaleAddress, "sale address not saved properly");
+			assert(expectedEndBlock === JSON.parse(actualEndBlock), "end block not saved properly");
 
 		}).timeout(300000);
-    });
 
-})
+	});
+
+	// distribute
+	describe('distribute private sale tokens', () => {
+		const beneficiaries = [web3.eth.accounts[18], web3.eth.accounts[19]];
+		const tokens = [5000000000, 400000000];
+		const timelocks = [1514678400, 1520035200, 1540035200];
+		const breakdown = [10, 30, 30];
+
+		it('filters address array should be populated', async () => {
+			await sale.distributeTimeLockedTokens(testSale.address, beneficiaries, tokens, timelocks, breakdown);
+			const expectedCount = timelocks.length;
+			let filter = testSale.filters(0);
+			console.log('resulting filter[0] value' + filter);
+			assert(expectedCount === filters.length, 'filter addresses were not populated correctly');
+		}).timeout(300000);
+
+
+	});
+
+/*
+	// setwallet
+	describe('distribute private sale tokens', () => {
+
+		await sale.distributeTimeLockedTokens(testSale.address, [web3.eth.accounts[18], web3.eth.accounts[19]], [5000000000, 400000000], [1514678400, 1520035200, 1540035200], [10, 30,30]);
+
+	})
+
+	//mark setup complete
+	describe('distribute private sale tokens', () => {
+
+		await sale.distributeTimeLockedTokens(testSale.address, [web3.eth.accounts[18], web3.eth.accounts[19]], [5000000000, 400000000], [1514678400, 1520035200, 1540035200], [10, 30,30]);
+
+	})
+
+	// add whitelists
+	describe('distribute private sale tokens', () => {
+
+		await sale.distributeTimeLockedTokens(testSale.address, [web3.eth.accounts[18], web3.eth.accounts[19]], [5000000000, 400000000], [1514678400, 1520035200, 1540035200], [10, 30,30]);
+
+	})
+
+	// lots of purchases
+
+	describe('distribute private sale tokens', () => {
+
+		await sale.distributeTimeLockedTokens(testSale.address, [web3.eth.accounts[18], web3.eth.accounts[19]], [5000000000, 400000000], [1514678400, 1520035200, 1540035200], [10, 30,30]);
+
+	})
+
+	// lock unsold
+	describe('distribute private sale tokens', () => {
+
+		await sale.distributeTimeLockedTokens(testSale.address, [web3.eth.accounts[18], web3.eth.accounts[19]], [5000000000, 400000000], [1514678400, 1520035200, 1540035200], [10, 30,30]);
+
+	})
+
+*/
+
+});
